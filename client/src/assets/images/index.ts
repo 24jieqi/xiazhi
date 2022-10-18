5f23a9a429f8d264ca87b46866d46ee2b9f0441c
limoer <myt19960503@outlook.com>
2022-10-18 13:36:52 +0800

    feat: 删除不必要的图片资源

--- client/src/assets/images/index.ts
+++ client/src/assets/images/index.ts
@@ -1,17 +1,1 @@
-import IconLogo from './icon_logo.png'
-import logo from './logo.png'
-import alert from './alert.png'
-import HomeBg from './bg.png'
-import AppLogo from './logo.jpg'
-import AppNewLogo from './new-logo.png'
-import Icon from './icon.png'
-
-export default {
-  IconLogo,
-  logo,
-  alert,
-  HomeBg,
-  AppLogo,
-  AppNewLogo,
-  Icon,
-}
+export {}
