86eab7849e35825413957eb7d1e63fab0ee030f2
unknown <myt19960503@outlook.com>
2024-04-19 17:12:30 +0800

    fix: 🐛 使用prettier格式化读取项目中配置实现优化

--- packages/ext-intl/src/utils/format.ts
+++ packages/ext-intl/src/utils/format.ts
@@ -1,17 +1,18 @@
 import * as chalk from 'chalk'
 import * as prettier from 'prettier'
+
+import { log } from './common'
 /**
  * 使用项目中的prettier配置进行格式化
  * @param text 需要格式化的文本
  * @param configFilePath 配置开始搜索的目录
  * @returns 格式化后的文本
  */
-export function formatFileWithConfig(
+export async function formatFileWithConfig(
   text: string,
   configFilePath?: string,
   parser: prettier.BuiltInParserName = 'typescript',
 ) {
-  const _configFilePath = configFilePath || process.cwd()
   let options: prettier.Options = {
     parser,
     bracketSpacing: true,
@@ -23,21 +24,21 @@ export function formatFileWithConfig(
     useTabs: true,
     proseWrap: 'never',
   }
-  try {
-    const configFinded = prettier.resolveConfig(_configFilePath)
+  const _configFilePath = configFilePath || (await prettier.resolveConfigFile())
+  if (!_configFilePath) {
+    log(
+      chalk.yellow(
+        '[WARNING] can not find perttier config file in your project, use default config instead!',
+      ),
+    )
+  } else {
+    const configFinded = await prettier.resolveConfig(_configFilePath!)
     if (configFinded) {
       options = {
         ...configFinded,
         parser,
       }
     }
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(
-      chalk.yellow(
-        '[WARNING] can not find perttier config file in your project, use default config instead!',
-      ),
-    )
   }
   return prettier.format(text, options)
 }
